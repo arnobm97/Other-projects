e5dbb2f9cfff6f507787c88425c58f56dcabb461
Arnob <arnobm97@gmail.com>
2021-09-11 19:13:16 +0600

    stuff added

---
+++ trial.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="style2.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php" style="font-size:2vw">Car Ventura</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" style="font-size:1vw" href="index.php"> Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" style="font-size:1vw" href="index.php"> Contact Us</a>
+        </li>
+  
+        <div class="dropdown show">
+  <a class="btn btn-secondary dropdown-toggle" href="#"  id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Vehicle List
+  </a>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <a class="dropdown-item" href="#">Mercedes-Benz</a>
+    <a class="dropdown-item" href="Mercedes.php">Rolls Royce</a>
+    <a class="dropdown-item" href="aston.php">Aston martin</a>
+    <a class="dropdown-item" href="#">Dodge Charger</a>
+    <a class="dropdown-item" href="bmw.php">BMW</a>
+  </div>
+</div>>
+        <li class="nav-item dropdown">
+         
+        </li>
+       
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+
+
+<div class="trial-container">
+    <div class= "row">
+        <div class ="col-lg-6 col-md-6 col-12">
+            <h2 class="py-3">Mercedes-Benz SLS AMG</h2>
+            <img src="assests/car.jpg" class="img-fluid aboutimg" >  
+            <div class="order">
+  <a href="#" class="btn btn-info" role="button">Order Now</a>
+ 
+ 
+</div>
+        </div>
+        <div>
+        <!-- <div class="trail-container"> -->
+  <div class="table-containter"> 
+        <div class="row">
+            <div class="col">
+            <h1 class="table-name">Specification</h1>
+            <table class="table">
+                        <tr class="table-header">
+                            <th>model</th>
+                            <th>speed</th>
+                            <th>speed</th>
+
+                        </tr>
+                            <tr class="table-header">
+                                <td>Classes:9<br>Student Capacity:80<br>Current Students:70</td>
+                                <td>Classes:9<br>Student Capacity:80<br>Current Students:70</td>
+                                <td>Classes:9<br>Student Capacity:80<br>Current Students:70</td>
+                            </tr>
+                            <tr class="table-header">
+                                <td>Classes:10<br>Student Capacity:80<br>Current Students:70</td>
+                                <td>Classes:10<br>Student Capacity:80<br>Current Students:70</td>
+                                <td>Classes:10<br>Student Capacity:80<br>Current Students:70</td>
+                            </tr >
+                            <tr class="table-header">
+                                <td>Classes:9<br>Student Capacity:80<br>Current Students:70</td>
+                                <td>Classes:9<br>Student Capacity:80<br>Current Students:70</td>
+                                <td>Classes:9<br>Student Capacity:80<br>Current Students:70</td>
+                            </tr>
+                    </table>
+                    <!-- <div class="color">
+             colors:<img src="assests/cyan.jpg"><div class="middle-text">
+                <div class="actual-text">Arnob Mallick<br>The most brilliant <br>01xxxxxxxx</div>
+             </div><img src="assests/cyan.jpg"><img src="assests/cyan.jpg"><img src="assests/cyan.jpg">
+              -->
+              <h5 class="t-header">Colors</h5>
+<div class="t-main-container">
+<div class="col"><div class="wrapping">
+<div class="t-container">
+    <div class="col">
+<img src="assests/cyan.jpg"  class="t-image" >
+    <div class="middle-text">
+    <div class="actual-text"><img src="assests/car.jpg"  class="car-image1" ></div>
+    </div>
+</div>
+</div>
+</div></div>
+<div class="col"><div class="wrapping">
+<div class="t-container">
+    <div class="col">
+<img src="assests/cyan.jpg"  class="t-image" >
+    <div class="middle-text">
+        <div class="actual-text"><img src="assests/car.jpg"  class="car-image1" ></div>
+    </div>
+</div>
+</div>
+</div></div>
+        <div class="col"><div class="wrapping">
+<div class="t-container">
+    <div class="col">
+<img src="assests/cyan.jpg"  class="t-image">
+    <div class="middle-text">
+    <div class="actual-text"><img src="assests/car.jpg"  class="car-image1" ></div>
+    </div>
+</div>
+</div>
+</div></div>
+</div>
+            </div>
+            </div>
+            </div>
+            <p1> write something</p1>
+            </div>    
+            
+
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
